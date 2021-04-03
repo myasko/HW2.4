89078c9ab2c574675f42e6b822a1b773ee13fd77
george <m9sssko@gmail.com>
2021-04-03 12:22:13 +0300

    Initial Commit

---
+++ HW2.4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW2.4
+//
+//  Created by Георгий Бутров on 03.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
